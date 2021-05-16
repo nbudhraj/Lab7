feaef9d88fe5e9c50a551abe516660b4e4a550da
nbudhraj <nbudhraj@ucsd.edu>
2021-05-15 23:00:27 -0700

    finished lab6

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,61 @@
 // script.js
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
+const body = document.querySelector("body");
+const title = document.querySelector("h1");
+
 
 // Make sure you register your service worker here too
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:3000/entries')
+  fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
         document.querySelector('main').appendChild(newPost);
+
+        newPost.addEventListener('click', () => {
+          var num = Array.prototype.indexOf.call(document.querySelector('main').children, newPost);
+          router.setState("entry",num+1, newPost);
+        });
       });
     });
 });
+
+
+const settingButton = document.querySelector("[alt='settings']");
+settingButton.addEventListener('click', () => { 
+  router.setState("settings");
+});
+
+title.addEventListener('click', () => { 
+  router.setState("home");
+ 
+});
+
+window.onpopstate = function(event){
+  if(event.state == null || event.state.page == "home"){
+    router.setState("home");
+  } else if(event.state.page == "settings"){
+    router.setState("settings"); 
+  } else {
+    router.setState("entry", event.state.page, document.querySelector('main').children[event.state.page-1]);
+    
+  }
+}
+
--- sw.js
+++ sw.js
@@ -4,3 +4,46 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
